266ef930351b2eb2fc3bc5d2f240355a76dcb13f
Henno Théo <zoupeurletroll@gmail.com>
2023-03-21 19:59:14 +0100

    player animation choose

--- source/games/player.lua
+++ source/games/player.lua
@@ -15,18 +15,28 @@ function Player:init(i, j, player)
 
     self.velocity = playdate.geometry.vector2D.new(0, 0)
     self.moveInputs = playdate.geometry.vector2D.new(0, 0)
+    self.lastDirection = "Bot"
 
     local playerShift = playerNumber == P1 and 0 or 5
-    local speed = 10
+    local tickSpeed = 10
 
     -- Colliders
     self:setCollideRect(10, 18, 12, 12)
     self:setCollidesWithGroups({ collisionGroup.block, collisionGroup.shiftBlock })
 
     -- Animation
-    self:addState('IdleDown', 19 + playerShift, 19 + playerShift, {
-        tickStep = speed
+    self:addState('IdleBot', 19 + playerShift, 19 + playerShift, {
+        tickStep = tickSpeed
     }).asDefault()
+    self:addState('IdleTop', 1 + playerShift, 1 + playerShift, {
+        tickStep = tickSpeed
+    })
+    self:addState('IdleRight', 10 + playerShift, 10 + playerShift, {
+        tickStep = tickSpeed
+    })
+    self:addState('IdleLeft', 28 + playerShift, 28 + playerShift, {
+        tickStep = tickSpeed
+    })
 
     self:playAnimation()
 
@@ -47,7 +57,21 @@ function Player:update()
     local velocity = self.moveInputs * self.speed
     self:moveWithCollisions(self.x + velocity.x, self.y + velocity.y)
 
-    print(self.moveInputs)
+    if velocity.y > 0 then
+        self:changeState('IdleBot', true)
+        self.lastDirection = "Bot"
+    elseif velocity.y < 0 then
+        self:changeState('IdleTop', true)
+        self.lastDirection = "Top"
+    elseif velocity.x < 0 then
+        self:changeState('IdleLeft', true)
+        self.lastDirection = "Left"
+    elseif velocity.x > 0 then
+        self:changeState('IdleRight', true)
+        self.lastDirection = "Right"
+    else 
+        self:changeState('Idle' .. self.lastDirection, true)
+    end
 
     self.moveInputs = playdate.geometry.vector2D.new(0, 0)
 end
